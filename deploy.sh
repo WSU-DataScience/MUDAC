e807bd3971d581abd8746495d2eee52f24aad7bd
Todd Iverson <tiverson@smumn.edu>
2018-02-13 10:25:04 -0600

    Added a nojekyll file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 git checkout master
-git subtree split --prefix build/stat110 -b gh-pages
+git subtree split --prefix public -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
